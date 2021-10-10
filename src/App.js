7c5798f9ccede484afdf17a50801957ac13d1dff
Lexfer <lexferramirez@gmail.com>
2021-10-10 08:57:18 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React,{useRef, useEffect} from 'react'
+import PropTypes from 'prop-types'
+
+ const App = ({name}) => {
+
+    const elementRef = useRef();
+
+    useEffect(() => {
+        //Usando useRef para referenciar un elemento del DOM
+        const divElement = elementRef.current;
+        console.log(divElement)
+    },[])
+
+    return (
+        <div ref={elementRef}>
+            helloo {name}
+        </div>
+    )
+}
+
+
+App.propTypes = {
+    name: PropTypes.string.isRequired,
+}
+
+App.defaultProps = {
+    name:"Lexfer Ramirez"
+}
+
+export default App
